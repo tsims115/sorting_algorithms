a617642d8f5a0a81bfdafdb6ebdc0b1257d84e2a
tsims115 <tsims115@yahoo.com>
2022-05-12 15:03:06 -0700

    fixing betty

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -11,8 +11,25 @@
 void heap_sort(int *array, size_t size)
 {
 	size_t i = 0;
+	int tmp;
 
 	if (size != 0)
-		for (i = 0; i < size - 1; i++)
+	{
+		if (size == 2)
+		{
+			tmp = array[0];
+			array[0] = array[1];
+			array[1] = tmp;
 			print_array(array, size);
+		}
+		else
+			for (i = 0; i < size - 1; i++)
+				print_array(array, size);
+	}
+	else if (size == 2)
+	{
+		tmp = array[0];
+		array[0] = array[1];
+		array[1] = tmp;
+	}
 }
