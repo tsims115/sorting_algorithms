0ed300fb906bc8b04dada5313c0bfa328799205a
tsims115 <tsims115@yahoo.com>
2022-05-12 14:46:34 -0700

    Adding sort,h and heapsort

---
+++ 104-heap_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+#include <stdio.h>
+
+
+/**
+ * heap_sort - heap sorts an array
+ * 
+ * @array: array to sort
+ * @size: size of the array
+ */
+void heap_sort(int *array, size_t size)
+{
+	print_array(array, size);
+}
--- sort.h
+++ sort.h
@@ -30,6 +30,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
 
 
 #endif /* SORT */
